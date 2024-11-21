c47b2469d0c03ff386d11091da9f8654997508c9
Caiden7-iu <MasonCaiden@Yahoo.com>
2024-11-21 18:11:34 -0500

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,22 @@
-function modelPopUp() {
-    // Your code here.
-}   
+document.addEventListener("DOMContentLoaded", () => {
+   
+   const openModalButton= document.getElementById("openModal");
+    const modal = document.getElementById("myModal");
+    const closeModalButton= document.getElementById("closeModal");
+    
+    openModalButton.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+    closeModalButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+    
+    window.addEventListener("click", (event) => {
+       
+        if (event.target == modal){
+            modal.style.display = "none";
+        }
+
+          
+    });
+});
